44e206ab2f5b0ae0bbd367b229ef6b332d97143e
Jaerock Kwon <jaerock.kwon@gmail.com>
2020-10-14 23:03:54 -0400

    add bimi.launch

--- start_rover.sh
+++ start_rover.sh
@@ -4,4 +4,4 @@ DONT_RUN=1 make px4_sitl_default gazebo
 
 source ./setup.bash
 
-roslaunch px4 mavros_posix_sitl.launch vehicle:='rover'
+roslaunch px4 mavros_posix_sitl_bimi.launch vehicle:='rover' rviz:='true' x:=3.0626 y:=-2.55103 z:=0.156312 R:=-0.002909 P:=0.026229 Y:=-0.641085
