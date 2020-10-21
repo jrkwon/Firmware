c998cef460ffe5e2cf0bdea33adc027ee258e7c4
Jaerock Kwon <jaerock.kwon@gmail.com>
2020-10-20 23:17:08 -0400

    change setup.bash

--- setup.bash
+++ setup.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-source ../catkin_ws/devel/setup.bash
 source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default > /dev/null 2>&1
 
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd) > /dev/null 2>&1
